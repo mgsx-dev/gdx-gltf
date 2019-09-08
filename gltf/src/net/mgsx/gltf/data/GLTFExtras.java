4b30b07861be969c8ee68abe21a030b97ae9c985
Germain AUBERT <germain.aubert@gmail.com>
2019-09-08 15:51:38 +0200

    improved custom properties access

--- gltf/src/net/mgsx/gltf/data/GLTFExtras.java
+++ gltf/src/net/mgsx/gltf/data/GLTFExtras.java
@@ -1,5 +1,6 @@
 package net.mgsx.gltf.data;
 
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.Json.Serializable;
 import com.badlogic.gdx.utils.JsonValue;
@@ -16,5 +17,27 @@ public class GLTFExtras implements Serializable
 	@Override
 	public void read(Json json, JsonValue jsonData) {
 		value = jsonData;
+	}
+	
+	/**
+	 * @return a new array of extra properties keys
+	 */
+	public Array<String> keys(){
+		Array<String> keys = new Array<String>();
+		for (JsonValue entry = value.child; entry != null; entry = entry.next) {
+			keys.add(entry.name);
+		}
+		return keys;
+	}
+	
+	/**
+	 * @return a new array of extra properties
+	 */
+	public Array<JsonValue> entries(){
+		Array<JsonValue> entries = new Array<JsonValue>();
+		for (JsonValue entry = value.child; entry != null; entry = entry.next) {
+			entries.add(entry);
+		}
+		return entries;
 	}
 }
