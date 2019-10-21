6af5b1df177072f16afe70e410ab9f8968ba27f5
Germain AUBERT <germain.aubert@gmail.com>
2019-10-21 18:13:53 +0200

    migrate to libgdx 1.9.10 and GWT 2.8.2

--- gltf/src/net/mgsx/gltf/scene3d/model/ModelInstanceHack.java
+++ gltf/src/net/mgsx/gltf/scene3d/model/ModelInstanceHack.java
@@ -24,66 +24,59 @@ public class ModelInstanceHack extends ModelInstance
 
 	public ModelInstanceHack(Model model) {
 		super(model);
-		// patch animation copy because of private method
-		animations.clear();
-		copyAnimations(model.animations, defaultShareKeyframes);
 	}
 	
 	public ModelInstanceHack(Model model, final String... rootNodeIds){
 		super(model, rootNodeIds);
-		// patch animation copy because of private method
-		animations.clear();
-		copyAnimations(model.animations, defaultShareKeyframes);
 	}
 	
-	private void copyAnimations (final Iterable<Animation> source, boolean shareKeyframes) {
-		for (final Animation anim : source) {
-			Animation animation = new Animation();
-			animation.id = anim.id;
-			animation.duration = anim.duration;
-			for (final NodeAnimation nanim : anim.nodeAnimations) {
-				final Node node = getNode(nanim.node.id);
-				if (node == null) continue;
-				NodeAnimationHack nodeAnim = new NodeAnimationHack();
-				nodeAnim.node = node;
-				
-				nodeAnim.translationMode = ((NodeAnimationHack)nanim).translationMode;
-				nodeAnim.rotationMode = ((NodeAnimationHack)nanim).rotationMode;
-				nodeAnim.scalingMode = ((NodeAnimationHack)nanim).scalingMode;
-				nodeAnim.weightsMode = ((NodeAnimationHack)nanim).weightsMode;
-				
-				if (shareKeyframes) {
-					nodeAnim.translation = nanim.translation;
-					nodeAnim.rotation = nanim.rotation;
-					nodeAnim.scaling = nanim.scaling;
-					nodeAnim.weights = ((NodeAnimationHack)nanim).weights;
-				} else {
-					if (nanim.translation != null) {
-						nodeAnim.translation = new Array<NodeKeyframe<Vector3>>();
-						for (final NodeKeyframe<Vector3> kf : nanim.translation)
-							nodeAnim.translation.add(new NodeKeyframe<Vector3>(kf.keytime, kf.value));
-					}
-					if (nanim.rotation != null) {
-						nodeAnim.rotation = new Array<NodeKeyframe<Quaternion>>();
-						for (final NodeKeyframe<Quaternion> kf : nanim.rotation)
-							nodeAnim.rotation.add(new NodeKeyframe<Quaternion>(kf.keytime, kf.value));
-					}
-					if (nanim.scaling != null) {
-						nodeAnim.scaling = new Array<NodeKeyframe<Vector3>>();
-						for (final NodeKeyframe<Vector3> kf : nanim.scaling)
-							nodeAnim.scaling.add(new NodeKeyframe<Vector3>(kf.keytime, kf.value));
-					}
-					if (((NodeAnimationHack)nanim).weights != null) {
-						((NodeAnimationHack)nanim).weights = new Array<NodeKeyframe<WeightVector>>();
-						for (final NodeKeyframe<WeightVector> kf : ((NodeAnimationHack)nanim).weights)
-							((NodeAnimationHack)nanim).weights.add(new NodeKeyframe<WeightVector>(kf.keytime, kf.value));
-					}
+	@Override
+	public void copyAnimation (Animation anim, boolean shareKeyframes) {
+		Animation animation = new Animation();
+		animation.id = anim.id;
+		animation.duration = anim.duration;
+		for (final NodeAnimation nanim : anim.nodeAnimations) {
+			final Node node = getNode(nanim.node.id);
+			if (node == null) continue;
+			NodeAnimationHack nodeAnim = new NodeAnimationHack();
+			nodeAnim.node = node;
+			
+			nodeAnim.translationMode = ((NodeAnimationHack)nanim).translationMode;
+			nodeAnim.rotationMode = ((NodeAnimationHack)nanim).rotationMode;
+			nodeAnim.scalingMode = ((NodeAnimationHack)nanim).scalingMode;
+			nodeAnim.weightsMode = ((NodeAnimationHack)nanim).weightsMode;
+			
+			if (shareKeyframes) {
+				nodeAnim.translation = nanim.translation;
+				nodeAnim.rotation = nanim.rotation;
+				nodeAnim.scaling = nanim.scaling;
+				nodeAnim.weights = ((NodeAnimationHack)nanim).weights;
+			} else {
+				if (nanim.translation != null) {
+					nodeAnim.translation = new Array<NodeKeyframe<Vector3>>();
+					for (final NodeKeyframe<Vector3> kf : nanim.translation)
+						nodeAnim.translation.add(new NodeKeyframe<Vector3>(kf.keytime, kf.value));
 				}
-				if (nodeAnim.translation != null || nodeAnim.rotation != null || nodeAnim.scaling != null || ((NodeAnimationHack)nanim).weights != null)
-					animation.nodeAnimations.add(nodeAnim);
+				if (nanim.rotation != null) {
+					nodeAnim.rotation = new Array<NodeKeyframe<Quaternion>>();
+					for (final NodeKeyframe<Quaternion> kf : nanim.rotation)
+						nodeAnim.rotation.add(new NodeKeyframe<Quaternion>(kf.keytime, kf.value));
+				}
+				if (nanim.scaling != null) {
+					nodeAnim.scaling = new Array<NodeKeyframe<Vector3>>();
+					for (final NodeKeyframe<Vector3> kf : nanim.scaling)
+						nodeAnim.scaling.add(new NodeKeyframe<Vector3>(kf.keytime, kf.value));
+				}
+				if (((NodeAnimationHack)nanim).weights != null) {
+					((NodeAnimationHack)nanim).weights = new Array<NodeKeyframe<WeightVector>>();
+					for (final NodeKeyframe<WeightVector> kf : ((NodeAnimationHack)nanim).weights)
+						((NodeAnimationHack)nanim).weights.add(new NodeKeyframe<WeightVector>(kf.keytime, kf.value));
+				}
 			}
-			if (animation.nodeAnimations.size > 0) animations.add(animation);
+			if (nodeAnim.translation != null || nodeAnim.rotation != null || nodeAnim.scaling != null || ((NodeAnimationHack)nanim).weights != null)
+				animation.nodeAnimations.add(nodeAnim);
 		}
+		if (animation.nodeAnimations.size > 0) animations.add(animation);
 	}
 	
 	@Override
