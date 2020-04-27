a3c3e4c1c25f42a8ebf1cef2cee9ccbcd65db857
Germain AUBERT <germain.aubert@gmail.com>
2020-04-27 19:22:40 +0200

    fix data structure for export

--- gltf/src/net/mgsx/gltf/data/data/GLTFBufferView.java
+++ gltf/src/net/mgsx/gltf/data/data/GLTFBufferView.java
@@ -3,6 +3,6 @@ package net.mgsx.gltf.data.data;
 import net.mgsx.gltf.data.GLTFEntity;
 
 public class GLTFBufferView extends GLTFEntity {
-	public int buffer, byteOffset = 0, byteLength;
-	public Integer byteStride, target;
+	public int byteOffset = 0, byteLength;
+	public Integer buffer, byteStride, target;
 }
