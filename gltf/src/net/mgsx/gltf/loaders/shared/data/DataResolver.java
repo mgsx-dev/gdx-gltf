be9597b62e0b9587f01de564cfea5417b31d2924
Mik <mik@classx.it>
2022-09-30 20:07:08 +0200

    mod: support for USHORT UBYTE bone weights (used from Unreal exporter)

--- gltf/src/net/mgsx/gltf/loaders/shared/data/DataResolver.java
+++ gltf/src/net/mgsx/gltf/loaders/shared/data/DataResolver.java
@@ -1,3 +1,4 @@
+
 package net.mgsx.gltf.loaders.shared.data;
 
 import java.nio.ByteBuffer;
@@ -11,16 +12,16 @@ import net.mgsx.gltf.data.data.GLTFBufferView;
 import net.mgsx.gltf.loaders.shared.GLTFTypes;
 
 public class DataResolver {
-	
+
 	private GLTF glModel;
 	private DataFileResolver dataFileResolver;
-	
+
 	public DataResolver(GLTF glModel, DataFileResolver dataFileResolver) {
 		super();
 		this.glModel = glModel;
 		this.dataFileResolver = dataFileResolver;
 	}
-	
+
 	public GLTFAccessor getAccessor(int accessorID) {
 		return glModel.accessors.get(accessorID);
 	}
@@ -28,34 +29,52 @@ public class DataResolver {
 	public float[] readBufferFloat(int accessorID) {
 		GLTFAccessor accessor = glModel.accessors.get(accessorID);
 		FloatBuffer floatBuffer = getBufferFloat(accessorID);
-		float [] data = new float[GLTFTypes.accessorSize(accessor) / 4];
+		float[] data = new float[GLTFTypes.accessorSize(accessor) / 4];
 		floatBuffer.get(data);
 		return data;
 	}
-	
+
 	public int[] readBufferUByte(int accessorID) {
 		GLTFAccessor accessor = glModel.accessors.get(accessorID);
 		GLTFBufferView bufferView = glModel.bufferViews.get(accessor.bufferView);
 		ByteBuffer bytes = dataFileResolver.getBuffer(bufferView.buffer);
 		bytes.position(bufferView.byteOffset + accessor.byteOffset);
-		int [] data = new int[GLTFTypes.accessorSize(accessor)];
-		for(int i=0 ; i<data.length ; i++){
+		int[] data = new int[GLTFTypes.accessorSize(accessor)];
+		for (int i = 0; i < data.length; i++) {
 			data[i] = bytes.get() & 0xFF;
 		}
 		return data;
 	}
-	
+
 	public int[] readBufferUShort(int accessorID) {
 		GLTFAccessor accessor = glModel.accessors.get(accessorID);
 		GLTFBufferView bufferView = glModel.bufferViews.get(accessor.bufferView);
 		ByteBuffer bytes = dataFileResolver.getBuffer(bufferView.buffer);
 		bytes.position(bufferView.byteOffset + accessor.byteOffset);
 		ShortBuffer shorts = bytes.asShortBuffer();
-		int [] data = new int[GLTFTypes.accessorSize(accessor)/2];
-		for(int i=0 ; i<data.length ; i++){
+		int[] data = new int[GLTFTypes.accessorSize(accessor) / 2];
+		for (int i = 0; i < data.length; i++) {
 			data[i] = shorts.get() & 0xFFFF;
 		}
 		return data;
+	}
+
+	public float[] readBufferUShortAsFloat(int accessorID) {
+		int[] intBuffer = readBufferUShort(accessorID);
+		float[] floatBuffer = new float[intBuffer.length];
+		for (int i = 0; i < intBuffer.length; i++) {
+			floatBuffer[i] = intBuffer[i] / 65535f;
+		}
+		return floatBuffer;
+	}
+
+	public float[] readBufferUByteAsFloat(int accessorID) {
+		int[] intBuffer = readBufferUByte(accessorID);
+		float[] floatBuffer = new float[intBuffer.length];
+		for (int i = 0; i < intBuffer.length; i++) {
+			floatBuffer[i] = intBuffer[i] / 255f;
+		}
+		return floatBuffer;
 	}
 
 	public FloatBuffer getBufferFloat(int accessorID) {
