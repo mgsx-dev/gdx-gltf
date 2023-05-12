ccfc8af6815746760be8f55298a29956b3702fb0
Germain AUBERT <germain.aubert@gmail.com>
2023-05-12 14:28:16 +0200

    fix GLTF exporter and cleanups, fixes #103

--- gltf/src/net/mgsx/gltf/loaders/shared/geometry/MeshLoader.java
+++ gltf/src/net/mgsx/gltf/loaders/shared/geometry/MeshLoader.java
@@ -24,7 +24,6 @@ import net.mgsx.gltf.data.geometry.GLTFMesh;
 import net.mgsx.gltf.data.geometry.GLTFPrimitive;
 import net.mgsx.gltf.loaders.blender.BlenderShapeKeys;
 import net.mgsx.gltf.loaders.exceptions.GLTFIllegalException;
-import net.mgsx.gltf.loaders.exceptions.GLTFRuntimeException;
 import net.mgsx.gltf.loaders.exceptions.GLTFUnsupportedException;
 import net.mgsx.gltf.loaders.shared.GLTFTypes;
 import net.mgsx.gltf.loaders.shared.data.DataResolver;
@@ -269,15 +268,6 @@ public class MeshLoader {
 					}
 					
 					GLTFBufferView glBufferView = dataResolver.getBufferView(glAccessor.bufferView);
-					
-					// not used for now : used for direct mesh ....
-					if(glBufferView.target != null){
-						if(glBufferView.target == 34963){ // ELEMENT_ARRAY_BUFFER
-						}else if(glBufferView.target == 34962){ // ARRAY_BUFFER
-						}else{
-							throw new GLTFRuntimeException("bufferView target unknown : " + glBufferView.target);
-						}
-					}
 					
 					FloatBuffer floatBuffer = dataResolver.getBufferFloat(glAccessor);
 					
